c5e458a51fefeaf355b05b5a5cc2e48e55148ab4
maedayuya <maeda.yuya.infratop@gmail.com>
2019-08-27 12:00:40 +0000

    customers devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+# ユーザー認証
+  devise_for :customers, controllers: {
+    registrations: 'customers/registrations',
+    sessions: "customers/sessions",
+    passwords: "customers/passwords"
+  }
+# ユーザーページ
+  resources :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
